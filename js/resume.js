b1ac37a8f93a1fec8106bee6fb97ed20ba6b9801
Frank Valdez <fvaldez421@gmail.com>
2018-04-24 17:36:05 -0700

    really fixed popover timeout this time...

--- js/resume.js
+++ js/resume.js
@@ -63,7 +63,7 @@
       var _this = this;
 
       setTimeout(function() {
-        if (($(".popover:hover").length) <= 0 ) {
+        if (($(".popover:hover").length) >= 0 ) {
           if (($(_this).is(":hover")) === false) {
             $(_this).popover("hide");
           }

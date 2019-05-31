65764c7da874683c60ce6c9862c004cca47cd76c
Frank Valdez <fvaldez421@gmail.com>
2019-05-30 21:39:11 -0700

    corrected popover issue, added mobile click popovers

--- js/resume.js
+++ js/resume.js
@@ -1,8 +1,15 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
+  var mobile = (window.matchMedia("(max-width: 992px)").matches) ? true : false;
+  var popoverActive = false;
+
+  function togglePopover(selector, action) {
+    if (!action) action = 'show';
+    $(selector).popover(action);
+  }
 
   // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -16,7 +23,7 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+  $('.js-scroll-trigger').click(function () {
     $('.navbar-collapse').collapse('hide');
   });
 
@@ -25,65 +32,79 @@
     target: '#sideNav'
   });
 
-  var mobile = function () {
-      if (!window.matchMedia) {
-        return false;
-      }
-      if (window.matchMedia("(max-width: 992px)").matches) {
-        return true;
-      }
-      return false;
-  }
-
 
   // Sets below variabes to pertaining project and runs popover
-  $(".link").hover(function() {
-    var trigger;
-    // var counter;
-    var id = $(this).attr("id");
-    var dep = $(this).attr("dep");
-    var git = $(this).attr("git");
-    var dBtn = "<a href=" + dep + " target='_blank'><button class='btn btn-success'>Deployed</button></a>" + " ";
-    var gBtn = "<a href=" + git + " target='_blank'><button class='btn btn-secondary'>GitHub</button></a>";
-    if (dep === undefined) { // conditionals for CLI or private repositories
-      dBtn = "";
-    }
-    if (git === undefined) {
-      gBtn = "";
-    }
-    if (mobile) {
-      trigger = "click";
-    }else {
-        trigger = "manual";
-    }
-
-    // Starts popover with manual trigger (mouseenter and mouseleave)
-    $("#" + id).popover({ 
-      trigger: "manual",
-      placement: "right",
-      html: true,
-      content: dBtn + gBtn,
-      container: "body"
-    }).on("mouseenter", function() {
-      var _this = this; 
-
-        if ($(_this).is(":hover") === true) {
-          $(_this).popover("show");
-        }
-        $(".popover").on("mouseleave", function() {
-          $("#" + id).popover("hide");
-        });
-
-    }).on("mouseleave", function() {
-      var _this = this;
-
-      setTimeout(function() {
-        if ($(".popover:hover").length <= 0 && !$(_this).is(":hover")) {
-            $(_this).popover("hide");
-        }
-      }, 200);
-
+  if (mobile) {
+    $(".link").on("click", function () {
+      var dep = $(this).attr("dep");
+      var git = $(this).attr("git");
+      var dBtn = "<a href=" + dep + " target='_blank'><button class='btn btn-success'>Deployed</button></a>" + " ";
+      var gBtn = "<a href=" + git + " target='_blank'><button class='btn btn-secondary'>GitHub</button></a>";
+      if (dep === undefined) { // conditionals for CLI or private repositories
+        dBtn = "";
+      }
+      if (git === undefined) {
+        gBtn = "";
+      }
+      $(this).popover({
+        trigger: "manual",
+        placement: "right",
+        html: true,
+        content: dBtn + gBtn,
+        container: "body"
+      });
+      var toggleAction = popoverActive ? "hide" : "show";
+      togglePopover(this, toggleAction);
+      popoverActive = !popoverActive;
     });
-  }); 
 
+  } else {
+    $(".link").hover(function () {
+      var dep = $(this).attr("dep");
+      var git = $(this).attr("git");
+      var dBtn = "<a href=" + dep + " target='_blank'><button class='btn btn-success'>Deployed</button></a>" + " ";
+      var gBtn = "<a href=" + git + " target='_blank'><button class='btn btn-secondary'>GitHub</button></a>";
+      if (dep === undefined) { // conditionals for CLI or private repositories
+        dBtn = "";
+      }
+      if (git === undefined) {
+        gBtn = "";
+      }
+
+      // Starts popover with manual trigger (mouseenter and mouseleave)
+      $(this).popover({
+        trigger: "manual",
+        placement: "right",
+        html: true,
+        content: dBtn + gBtn,
+        container: "body"
+      }).hover(function () {
+        if ($(this).is(":hover") === true) {
+          $(this).popover("show");
+          togglePopover(this, 'show')
+        }
+        $(".popover").on("mouseleave", function () {
+          togglePopover(this, "hide");
+        });
+      }).on("mouseenter", function () {
+        if ($(this).is(":hover") === true) {
+          $(this).popover("show");
+          togglePopover(this, 'show')
+        }
+        $(".popover").on("mouseleave", function () {
+          togglePopover(this, "hide");
+        });
+      }).on("mouseleave", function () {
+        var _this = this;
+
+        setTimeout(function () {
+          if ($(".popover:hover").length <= 0 && !$(_this).is(":hover")) {
+            togglePopover(".popover", "hide");
+          }
+        }, 200);
+
+      });
+      if ($(this).is(":hover")) togglePopover(this)
+    });
+  }
 })(jQuery); // End of use strict

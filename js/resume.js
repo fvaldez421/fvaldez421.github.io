0c72879ab6a15f1fa7cec1c3df9212c41492c782
Frank Valdez <fvaldez421@gmail.com>
2021-03-11 17:18:51 -0800

    updated with most recent exp, fixed popover js and some css bugs

--- js/resume.js
+++ js/resume.js
@@ -1,12 +1,5 @@
 (function ($) {
   "use strict"; // Start of use strict
-  var mobile = (window.matchMedia("(max-width: 992px)").matches) ? true : false;
-  var popoverActive = false;
-
-  function togglePopover(selector, action) {
-    if (!action) action = 'show';
-    $(selector).popover(action);
-  }
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
@@ -16,7 +9,7 @@
       if (target.length) {
         $('html, body').animate({
           scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
+        }, 1000, 'easeInOutExpo');
         return false;
       }
     }
@@ -33,79 +26,119 @@
   });
 
 
-  // Sets below variabes to pertaining project and runs popover
+  /** ____________ BEGIN POPOVER LOGIC ____________ */
+  var mobile = (window.matchMedia("(max-width: 992px)").matches) ? true : false;
+  var activePopover = null;
+  var ENUM_SITE = 'site',
+    ENUM_SOURCE = 'source',
+    ENUM_HIDE = 'hide',
+    ENUM_SHOW = 'show';
+
+  function togglePopover(selector, action) {
+    var tgt = $(selector);
+    if (action === undefined) {
+      action = activePopover ? ENUM_HIDE : ENUM_SHOW;
+    }
+    if (action === ENUM_HIDE) {
+      activePopover = null;
+    } else if (action === ENUM_SHOW) {
+      activePopover = tgt && tgt.id || true;
+    }
+    tgt.popover(action);
+    return tgt;
+  }
+
+  function closeAllPopovers() {
+    togglePopover('.link', ENUM_HIDE);
+  }
+
+  function makePopoverBtn(type, link, isProject) {
+    var label = '',
+      className = '';
+    if (type === ENUM_SITE) {
+      label = isProject ? 'Live Site' : 'Read More';
+      className = 'btn btn-primary';
+    } else if (type === ENUM_SOURCE) {
+      label = 'Source Code';
+      className = 'btn btn-success';
+    }
+    if (!!type && !!link) {
+      return "<a href=" + link + " target='_blank' class='" + className + "'>" + label + "</a>";
+    }
+    return '';
+  }
+
+  function makePopoverContent(_this) {
+    var siteLink = $(_this).attr('dep');
+    var sourceLink = $(_this).attr('git');
+    var isProject = !!($(_this).attr('project'));
+    var dBtn = makePopoverBtn(ENUM_SITE, siteLink, isProject);
+    var gBtn = makePopoverBtn(ENUM_SOURCE, sourceLink, isProject);
+    return dBtn + ' ' + gBtn;
+  }
+
+  function isLink(el) {
+    if (!!el) {
+      return el.hasAttribute('dep') || el.hasAttribute('git')
+    }
+  }
+
   if (mobile) {
-    $("#titleInst").html("(Click titles for options)")
-    $(".link").on("click", function () {
-      var dep = $(this).attr("dep");
-      var git = $(this).attr("git");
-      var dBtn = "<a href=" + dep + " target='_blank' class='btn btn-success'>Live</a>" + " ";
-      var gBtn = "<a href=" + git + " target='_blank' class='btn btn-secondary'>GitHub</a>";
-      if (dep === undefined) { // conditionals for CLI or private repositories
-        dBtn = "";
-      }
-      if (git === undefined) {
-        gBtn = "";
-      }
+    $('#titleInst').html('(Tap etry titles for options)')
+    $('.link').on('click', function () {
       $(this).popover({
-        trigger: "manual",
-        placement: "right",
+        trigger: 'manual',
+        placement: 'left',
         html: true,
-        content: dBtn + gBtn,
-        container: "body"
+        container: 'body',
+        content: makePopoverContent(this)
       });
-      var toggleAction = popoverActive ? "hide" : "show";
-      togglePopover(this, toggleAction);
-      popoverActive = !popoverActive;
+      closeAllPopovers();
+      togglePopover(this);
     });
 
+    $('body').on('click', function (evt) {
+      var isLinkClick = isLink(evt && evt.target)
+      if (activePopover && !isLinkClick) {
+        closeAllPopovers();
+      }
+    });
   } else {
-    $(".link").hover(function () {
-      var dep = $(this).attr("dep");
-      var git = $(this).attr("git");
-      var dBtn = "<a href=" + dep + " target='_blank' class='btn btn-success'>Live</a>" + " ";
-      var gBtn = "<a href=" + git + " target='_blank' class='btn btn-secondary'>GitHub</a>";
-      if (dep === undefined) { // conditionals for CLI or private repositories
-        dBtn = "";
-      }
-      if (git === undefined) {
-        gBtn = "";
-      }
-
+    $('.link').hover(function () {
       // Starts popover with manual trigger (mouseenter and mouseleave)
       $(this).popover({
-        trigger: "manual",
-        placement: "right",
+        trigger: 'manual',
+        placement: 'right',
         html: true,
-        content: dBtn + gBtn,
-        container: "body"
+        container: 'body',
+        content: makePopoverContent(this)
       }).hover(function () {
-        if ($(this).is(":hover") === true) {
-          $(this).popover("show");
-          togglePopover(this, 'show')
+        if ($(this).is(':hover') === true) {
+          $(this).popover(ENUM_SHOW);
+          togglePopover(this, ENUM_SHOW)
         }
-        $(".popover").on("mouseleave", function () {
-          togglePopover(this, "hide");
+        $('.popover').on('mouseleave', function () {
+          togglePopover(this, ENUM_HIDE);
         });
-      }).on("mouseenter", function () {
-        if ($(this).is(":hover") === true) {
-          $(this).popover("show");
-          togglePopover(this, 'show')
+      }).on('mouseenter', function () {
+        if ($(this).is(':hover') === true) {
+          $(this).popover(ENUM_SHOW);
+          togglePopover(this, ENUM_SHOW)
         }
-        $(".popover").on("mouseleave", function () {
-          togglePopover(this, "hide");
+        $('.popover').on('mouseleave', function () {
+          togglePopover(this, ENUM_HIDE);
         });
-      }).on("mouseleave", function () {
+      }).on('mouseleave', function () {
         var _this = this;
-
         setTimeout(function () {
-          if ($(".popover:hover").length <= 0 && !$(_this).is(":hover")) {
-            togglePopover(".popover", "hide");
+          if ($('.popover:hover').length <= 0 && !$(_this).is(':hover')) {
+            togglePopover('.popover', ENUM_HIDE);
           }
-        }, 200);
-
+        }, 150);
       });
-      if ($(this).is(":hover")) togglePopover(this)
+      if ($(this).is(':hover')) {
+        togglePopover(this);
+      }
     });
   }
 })(jQuery); // End of use strict
